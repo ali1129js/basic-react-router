90ab69d70e1ab60b953f52b052dc6dcd0b4904ac
ali1129js <ali1129js@gmail.com>
2018-04-15 20:56:17 +0200

    added browser router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-15T20:30:45+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-15T20:32:38+02:00
+ */
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+  , document.getElementById('root'));
 registerServiceWorker();
