0c59eadfa509732250cd137c501663699d0138dd
ali1129js <ali1129js@gmail.com>
2018-04-22 01:24:24 +0200

    3 components , 3 links and 3 routes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,52 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-15T16:34:41+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-22T01:06:38+02:00
+ * @Last modified time: 2018-04-22T01:22:57+02:00
  */
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom'
+ import React, { Component } from 'react'
+ import {
+   BrowserRouter as Router,
+   Route,
+   Link
+ } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        hi
-      </div>
-    );
-  }
-}
+ const Home = () => (
+   <div>
+     <h2>Home</h2>
+   </div>
+ )
 
-export default App;
+ const About = () => (
+   <div>
+     <h2>About</h2>
+   </div>
+ )
+
+ const Topics = ({ match }) => (
+   <div>
+     <h2>Topics</h2>
+   </div>
+ )
+
+ class App extends Component {
+   render() {
+     return (
+       <Router>
+         <div>
+           <ul>
+             <li><Link to="/">Home</Link></li>
+             <li><Link to="/about">About</Link></li>
+             <li><Link to="/topics">Topics</Link></li>
+           </ul>
+
+           <hr/>
+           <Route exact path="/" component={Home}/>
+           <Route path="/about" component={About}/>
+           <Route path="/topics" component={Topics}/>
+         </div>
+       </Router>
+     )
+   }
+ }
+
+ export default App
