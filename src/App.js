23f1805d1ddbe50774e2665ccc3db5cbb64319ce
ali1129js <ali1129js@gmail.com>
2018-04-22 01:07:24 +0200

    new tuts

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-15T16:34:41+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-15T16:34:41+02:00
+ * @Last modified time: 2018-04-22T01:06:38+02:00
  */
-
-
-
 import React, { Component } from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        hi
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-15T20:30:45+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-15T20:32:38+02:00
+ * @Last modified time: 2018-04-22T01:05:57+02:00
  */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
-  , document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
